ecba9738afc7f84fa9adeafc5a7d51fb8841dd2c
James Farr <james.farr@jdplc.com>
2024-05-08 11:55:49 +0100

    Top banner styling added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import BaseTemplate from './ui/slots/BaseTemplate.vue';
+import TopNav from './components/TopNav.vue';
 </script>
 
 <template>
   <BaseTemplate>
+    <TopNav />
     <h1 class="text-3xl underline font-sans">
       Hello world!
     </h1>
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,3 +1,11 @@
 <template>
-    
+    <div class="flex bg-secondaryGray h-10">
+        <div class="flex text-xs items-center">
+            <a class="py-8 px-5 font-semibold" href="#">Business lines</a>
+            <a class="py-8 px-5 font-semibold" href="#">Global brands</a>
+        </div>
+        <div>
+
+        </div>
+    </div>
 </template>
