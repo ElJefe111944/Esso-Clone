69608e928c14c7945e5fc2b5887a39cc36e593e5
James Farr <james.farr@jdplc.com>
2024-05-08 14:05:11 +0100

    Top nav links added and styled

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,17 +1,26 @@
 <template>
-    <div class="flex bg-secondaryGray h-10">
+    <div class="flex bg-secondaryGray h-10 justify-between">
         <div class="flex text-xs items-center">
             <a class="pt-3 pb-3 px-4 font-medium hover:bg-hoverGray" href="#">Business lines</a>
             <a class="pt-3 pb-3 px-4 font-medium hover:bg-hoverGray" href="#">Global brands</a>
         </div>
-        <div>
+        <div class="flex items-center">
             <!-- social -->
-            <div>
-
+            <div class="flex text-xs">
+                <!-- desktop icons -->
+                <div class="hidden lg:flex items-center">
+                    <a class="pt-3 pb-3 px-4 font-medium hover:bg-hoverGray" href="#">Icons</a>
+                </div>
+                <!-- mobile icons -->
+                <div class="items-center lg:hidden">
+                    <a class="pt-3 pb-3 px-4 font-medium hover:bg-hoverGray" href="#">Social</a>
+                </div>
             </div>
             <!-- location -->
             <div>
-
+                <div class="flex items-center text-xs">
+                    <a class="pt-3 pb-3 px-4 font-medium hover:bg-hoverGray" href="#">Select location</a>
+                </div>
             </div>
         </div>
     </div>
