6d0fb6ea140443ed4bf2623e618aacba8503064e
James Farr <james.farr@jdplc.com>
2024-05-09 16:07:47 +0100

    Navbar component added

--- src/ui/slots/TopNavLink.vue
+++ src/ui/slots/TopNavLink.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="mx-2 md:mx-0 md:text-xs md:px-4 flex h-full items-center font-medium hover:bg-hoverGray text-[10px] text-nowrap" href="#">
+    <a class="mx-2 md:mx-0 md:text-xs md:px-4 flex h-full items-center no-underline text-primaryBlack font-medium hover:bg-hoverGray text-[9px] text-nowrap" href="#">
         <slot></slot>
     </a>
 </template>
