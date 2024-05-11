6459bc0fd54b9224e6388d43ff94ec83e46a5c66
James Farr <james.farr@jdplc.com>
2024-05-11 19:27:23 +0100

    Mobile toggle icons styled and navbar complete

--- src/components/main-nav/NavBar.vue
+++ src/components/main-nav/NavBar.vue
@@ -1,25 +1,49 @@
 <template>
   <nav class="navbar bg-body-tertiary p-0">
-    <div class="container-fluid h-20 lg:px-0">
+    <div class="container-fluid h-20 lg:px-0 pr-0 flex-nowrap">
       <a class="navbar-brand lg:pl-4" href="#">
         <img class="md:w-full w-[140px] h-auto max-w-full md:ml-4" :src="Logo" alt="Esso Logo">
       </a>
-      <button class="navbar-toggler block md:block lg:hidden" type="button" data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <div class="flex md:hidden lg:hidden items-center h-full">
+        <MainNavItem class="list-none flex flex-col justify-center gap-2 items-center" @click="searchDropdownOpen = !searchDropdownOpen">
+          <SearchIcon height="20" width="20" colour="rgb(14,70,155)" v-if="!searchDropdownOpen" />
+          <CloseIcon height="20" width="20" colour="rgb(14,70,155)" v-else />
+          <span class="text-sm">Search</span>
+        </MainNavItem>
+        <MainNavItem class="list-none flex flex-col justify-center gap-2 items-center">
+          <button class="navbar-toggler block md:block lg:hidden p-0 border-none shadow-none" type="button"
+            data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation">
+            <BurgerIcon height="20" width="20" colour="rgb(14,70,155)" />
+          </button>
+          <span class="text-sm">Menu</span>
+        </MainNavItem>
+      </div>
+
       <ul class="hidden lg:flex justify-items-end gap-x-0 items-center p-0 m-0 h-full">
         <MainNavItem>
-          Station Finder
+          <a class="items-center text-sm no-underline text-primaryBlack hover:text-primaryBlue my-auto font-medium text-nowrap flex h-full"
+            href="#">
+            Station Finder
+          </a>
         </MainNavItem>
         <MainNavItem>
-          Loyalty & Payments
+          <a class="items-center text-sm no-underline text-primaryBlack hover:text-primaryBlue my-auto font-medium text-nowrap flex h-full"
+            href="#">
+            Loyalty & Payments
+          </a>
         </MainNavItem>
         <MainNavItem>
-          Fuels
+          <a class="items-center text-sm no-underline text-primaryBlack hover:text-primaryBlue my-auto font-medium text-nowrap flex h-full"
+            href="#">
+            Fuels
+          </a>
         </MainNavItem>
         <MainNavItem>
-          Contact
+          <a class="items-center text-sm no-underline text-primaryBlack hover:text-primaryBlue my-auto font-medium text-nowrap flex h-full"
+            href="#">
+            Contact
+          </a>
         </MainNavItem>
         <li @click="searchDropdownOpen = !searchDropdownOpen"
           class="hover:bg-hoverGray hover:border-t-4 border-t-4 border-transparent hover:border-primaryBlue h-full px-4">
@@ -43,8 +67,11 @@
   <!-- search dropdown -->
   <div class="h-20 flex bg-body-tertiary w-full border-t-2 border-gray-200" v-show="searchDropdownOpen">
     <div class="flex-1 flex my-0 mx-2 md:mx-4 items-center gap-4">
-      <SearchIcon height="33" width="33" colour="rgb(158,158,158)" class="md:ml-2" stroke-width="2" stroke="lightGray" />
-      <input class="text-gray-600 text-xl w-full h-full outline-none placeholder:text-gray-600 placeholder:text-lg md:placeholder:text-2xl" placeholder="Search for topics, articles, data.." type="text">
+      <SearchIcon height="33" width="33" colour="rgb(158,158,158)" class="md:ml-2" stroke-width="2"
+        stroke="lightGray" />
+      <input
+        class="text-gray-600 text-xl w-full h-full outline-none placeholder:text-gray-600 placeholder:text-lg md:placeholder:text-2xl"
+        placeholder="Search for topics, articles, data.." type="text">
     </div>
     <div class="w-[200px] items-center justify-center border-l-2 border-gray-300 my-3 hidden md:flex">
       <button class="w-full text-gray-600 text-xl">Search</button>
@@ -59,6 +86,7 @@ import MainNavItem from '../../ui/slots/MainNavItem.vue';
 import SearchIcon from "../../ui/icons/SearchIcon.vue";
 import MobileNav from "./MobileNav.vue";
 import CloseIcon from "../../ui/icons/CloseIcon.vue";
+import BurgerIcon from "../../ui/icons/BurgerIcon.vue";
 
 
 const searchDropdownOpen = ref(false);
---
+++ src/ui/icons/BurgerIcon.vue
@@ -0,0 +1,27 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" :width="props.width" :height="props.height" :style="{ fill: props.colour }" class="bi bi-list"
+        viewBox="0 0 16 16">
+        <path fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+    </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+    colour: {
+        type: String,
+        required: false,
+        default: 'rgb(12, 71, 157)'
+    },
+    height: {
+        type: String,
+        required: false,
+        default: 16
+    },
+    width: {
+        type: String,
+        required: false,
+        default: 16
+    }
+})
+</script>
