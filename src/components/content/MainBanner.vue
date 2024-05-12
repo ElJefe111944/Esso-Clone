c6388159b439938053c2467d53652a5b0f0fddf6
James Farr <james.farr@jdplc.com>
2024-05-12 20:24:06 +0100

    Base button styling added for main banner

--- src/components/content/MainBanner.vue
+++ src/components/content/MainBanner.vue
@@ -3,19 +3,22 @@
         <div class="relative">
             <picture>
                 <source media="(min-width:768px)" srcset="https://www.esso.co.uk/-/media/eame/uk/images/promotion/esso-red/esso-red-screen-xl.jpg">
-                <img class="w-full" src="https://www.esso.co.uk/-/media/eame/uk/images/promotion/esso-red/esso-red-fs-xs.jpg" alt="" style="width:auto;">
+                <img class="w-full" src="https://www.esso.co.uk/-/media/eame/uk/images/promotion/esso-red/esso-red-fs-xs.jpg" alt="">
             </picture>
         </div>
         <div>
             <h1>Looking for fuel nearby?</h1>
             <p>We can help you find your nearest service station and even give you directions.</p>
             <p>
-                
+                <BaseButton>
+                    <a class="no-underline text-white text-base capitalize font-bold tracking-normal font-sans" href="#">Find a service station</a>
+                </BaseButton>
             </p>
         </div>
     </div>
 </template>
 
 <script setup>
+import BaseButton from '../../ui/slots/BaseButton.vue';
 
 </script>
---
+++ src/ui/slots/BaseButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-btn height="47" class="shadow-none bg-primaryBlue rounded-none px-12 py-[10px]">
+        <slot></slot>
+    </v-btn>
+</template>
+
+<style scoped>
+/* button.v-btn.v-btn--elevated.v-theme--light.v-btn--density-default.v-btn--size-default.v-btn--variant-elevated.shadow-none.bg-primaryBlue.rounded-none.px-12.py-\[10px\] {
+    height: 47px;
+} */
+</style>
