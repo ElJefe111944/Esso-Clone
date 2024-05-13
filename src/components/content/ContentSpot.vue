d0dd8ea2ff9c3f56c7a94ef10a520aa921bd44b5
James Farr <james.farr@jdplc.com>
2024-05-13 11:45:13 +0100

    Content spot mobile styling added

--- src/components/content/ContentSpot.vue
+++ src/components/content/ContentSpot.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex md:mt-8 lg:flex-row flex-col sm:mt-2">
+    <div class="flex md:mt-8 lg:flex-row flex-col sm:mt-2 gap-3">
         <div class="flex-1">
             <picture>
                 <source media="(min-width:768px)"
@@ -8,14 +8,14 @@
                     src="https://www.esso.co.uk/-/media/eame/uk/images/promotion/esso-red/esso-red-fs-xs.jpg" alt="">
             </picture>
         </div>
-        <div class="bg-white lg:px-4 flex-1 lg:py-3">
-            <h2 class="lg:text-4xl font-thin text-primaryGrayIcons font-sans md:text-2xl text-xl">3x cleaner engine
+        <div class="bg-white lg:px-4 flex-1">
+            <h2 class="lg:text-3xl font-thin text-primaryGrayIcons font-sans md:text-xl text-xl">3x cleaner engine
                 with new Esso Synergy Supreme</h2>
             <p class="lg:text-base font-sans font-thin text-sm">+ 99 triple cleans and protects your engine to give you
                 more miles per tank. Try it now and get 3x Nectar or Clubcard points.*</p>
             <p>
                 <BaseButton>
-                    <a class="no-underline text-white text-base capitalize font-bold tracking-normal font-sans"
+                    <a class="no-underline text-white text-sm capitalize font-bold tracking-normal font-sans"
                         href="#">Find out more about Esso fuels</a>
                 </BaseButton>
             </p>
--- src/ui/slots/BaseButton.vue
+++ src/ui/slots/BaseButton.vue
@@ -3,9 +3,3 @@
         <slot></slot>
     </v-btn>
 </template>
-
-<style scoped>
-/* button.v-btn.v-btn--elevated.v-theme--light.v-btn--density-default.v-btn--size-default.v-btn--variant-elevated.shadow-none.bg-primaryBlue.rounded-none.px-12.py-\[10px\] {
-    height: 47px;
-} */
-</style>
