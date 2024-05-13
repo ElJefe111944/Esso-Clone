f5d359e252600861c378cadae614792ed5164da9
James Farr <james.farr@jdplc.com>
2024-05-13 15:56:52 +0100

    Chevron icon component created

--- src/components/content/CardCopy.vue
+++ src/components/content/CardCopy.vue
@@ -6,17 +6,15 @@
         <p>
             <a class="text-primaryBlue font-semibold flex items-center underline" href="#">
                 {{ ctaText }}
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="rgb(14, 70, 155)"
-                    class="bi bi-chevron-right underline" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd"
-                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
-                </svg>
+                <ChevronIcon />
             </a>
         </p>
     </div>
 </template>
 
 <script setup>
+import ChevronIcon from '../../ui/icons/ChevronIcon.vue';
+
 const props = defineProps({
     card: {
         id: {
---
+++ src/ui/icons/ChevronIcon.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="rgb(14, 70, 155)"
+        class="bi bi-chevron-right underline" viewBox="0 0 16 16">
+        <path fill-rule="evenodd"
+            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+    </svg>
+</template>
