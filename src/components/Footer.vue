62cac1eaf83778db49cf4944286e3a658f447dee
James Farr <james.farr@jdplc.com>
2024-05-20 12:07:37 +0100

    Top section of footer styled

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <!-- links -->
-        <div class="flex bg-footerGray lg:flex-row flex-col w-full p-4 justify-between items-center">
-            <div class="flex w-2/3 text-white gap-3">
+        <div class="flex bg-footerGray lg:flex-row md:flex-col flex-col w-full p-4 justify-between items-center gap-3">
+            <div class="flex w-full md:w-2/3 text-white gap-3 flex-wrap">
                 <a class="text-white no-underline whitespace-nowrap text-sm font-medium" href="#">Contact Us</a>
                 <a class="text-white no-underline whitespace-nowrap text-sm font-medium" href="#">Mobile</a>
                 <a class="text-white no-underline whitespace-nowrap text-sm font-medium" href="#">Commercial customers</a>
@@ -10,8 +10,8 @@
                 <a class="text-white no-underline whitespace-nowrap text-sm font-medium" href="#">Esso App F&Qs</a>
                 <a class="text-white no-underline whitespace-nowrap text-sm font-medium" href="#">Tesco clubcard F&Qs</a>
             </div>
-            <div class="flex items-center relative">
-                <input class="flex bg-footerBlack h-11 px-2 w-[300px] md:w-[350px] placeholder:text-sm placeholder:font-sans placeholder:font-semibold" placeholder="Search" type="text">
+            <div class="flex items-center relative w-full md:w-auto">
+                <input class="flex bg-footerBlack h-11 px-2 w-full md:w-[350px] placeholder:text-sm placeholder:font-sans placeholder:font-semibold" placeholder="Search" type="text">
                 <SearchIcon class="absolute right-2" />
             </div>
         </div>
